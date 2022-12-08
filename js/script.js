c911563781e6ddd1806f51b044728ff5aca4f990
k0edb <jeanlamego@hotmail.com>
2022-12-08 20:32:14 -0300

    mais uma parte do html finalizado. iniciando o css

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// let moonInfo = document.querySelector('#planet-container');
+
+// fetch("data.json").then((response)=>{
+//     response.json().then((destinations)=>{
+//         console.log("destinations");
+//     })
+// })
