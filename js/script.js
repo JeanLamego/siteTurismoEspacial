7cba7f9fd16f12da1cccc0627355771ccd001957
k0edb <jeanlamego@hotmail.com>
2022-12-09 21:47:58 -0300

    sidebar toggle active

--- js/script.js
+++ js/script.js
@@ -5,3 +5,20 @@
 //         console.log("destinations");
 //     })
 // })
+
+const navBar = document.querySelector('.nav-bar');
+const closeBar = document.querySelector('.nav-button.mostra');
+
+// const closeBtn= addEventListener('click', function(){
+//     navBar.classList.toggle("mostra");
+// })
+const closeBtn=addEventListener('click', function(){
+    closeBar.classList.toggle("");
+})
+
+const openBtn= addEventListener('click', function(){
+navBar.classList.toggle("mostra");
+})
+// $('.menu-btn').click(function(){
+// $('.nav-bar').toggleClass('mostra')
+// });
