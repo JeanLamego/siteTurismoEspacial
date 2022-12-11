1786513bcda2f8fe5e78c78b83370d49f0e65ea3
k0edb <jeanlamego@hotmail.com>
2022-12-11 09:05:23 -0300

    progredindo com foreach

--- js/script.js
+++ js/script.js
@@ -1,14 +1,29 @@
 // let moonInfo = document.querySelector('#planet-container');
-
-// fetch("data.json").then((response)=>{
-//     response.json().then((destinations)=>{
-//         console.log("destinations");
-//     })
-// })
+const moonClick = document.querySelector('#moon')
 
 const navBar = document.querySelector('.nav-bar');
 const openBtn = document.querySelector('.menu-btn');
 const closeBtn = document.querySelector('.close-bar');
+
+
+
+fetch("../data.json").then((response)=>{
+    response.json().then((data)=>{
+        console.log(data.destinations);
+    },
+    )
+
+});
+
+
+
+
+
+document.querySelector
+
+// moonClick.addEventListener('click', function(){
+
+// })
 
 closeBtn.addEventListener('click', function(){
     navBar.classList.toggle("mostra");
